7d9e74520361932318c68a708fed225f3419fd8e
Brian Slaby <brian.slaby@gmail.com>
2023-10-31 20:36:12 -0400

    added rust monsters

--- data-bestiary.js
+++ data-bestiary.js
@@ -2767,11 +2767,135 @@ const bestiaryMonsterData = [
 
 // RUST MONSTERS
 
+{
+    name: "Rust Monster",
+    level: 3,
+    type: "spoiler",
+    initiative: "+5",
+    ac: 19,
+    pd: 17,
+    md: 13,
+    hp: 45,
+    health: 45,
+    attacks: ["Caustic Bite +8 vs AC (one creature wearing light armor or no armor) - 10 damage, and 5 ongoing acid damage. Natural 16+: The target is now considered to be wearing no armor (the armor is damaged beyond repair; adjust defenses accordingly). If the target's armor is magical, the target must roll a hard save. On a success, the armor isn't affected. This effect is permanent until the creature obtains new armor.", "Rusting Antenna +8 vs PD (one creature wearing heavy armor) - 1 damage, and the target is now considered to be wearing no armor (the armor is damaged beyond repair; adjust defenses accordingly). If the target's armor is magical, the target must roll a hard save. On a success, the armor isn't affected. This effect is permanent until the creature obtains new armor."],
+    abilities: ["Corrupting Body: Whenever a creature hits the rust monster with a melee weapon, the attacker must roll a save; on a failure, the weapon is destroyed. Magic weapons require an easy save instead. This effect is permanent.", "Rust's Targets: Magic items wielded by creatures with 60 HP or more are not affected by the rust monster's ability to destroy items (but the target still takes damage).", "Saving Quirks: Before rolling a save for a magic item, a PC can gain a +5 bonus to the save by agreeing to roleplay the item's quirk as a huge element of their personality until the next full heal up. Failure to live up to this roleplaying agreement means that the item hasn't received the support it needs from its owner to survive the rust monster's destructive effects, and the item is destroyed at the end of the next battle even if its owner successfully saved.", "[Nastier Special] Big Antenna: When the rust monster scores a critical hit, it can try to destroy another random magic item in the target's possession (or a random normal item if the target has no other magic items). The creature must roll a save for the item, as usual."],
+    isStaggered: false,
+    id: 291
+},
 
+{
+    name: "Rust Monster Obliterator",
+    level: 5,
+    type: "spoiler",
+    initiative: "+8",
+    ac: 21,
+    pd: 19,
+    md: 15,
+    hp: 72,
+    health: 72,
+    attacks: ["Caustic Bite +10 vs AC (one creature wearing light armor or no armor) - 18 damage, and 5 ongoing acid damage. Natural 14+: The target is now considered to be wearing no armor (the armor is damaged beyond repair; adjust defenses accordingly). If the target's armor is magical, the target must roll a hard save. On a success, the armor isn't affected. This effect is permanent until the creature obtains new armor.", "Rusting Antenna +10 vs PD (one creature wearing heavy armor) - 10 damage, and the target is now considered to be wearing no armor (the armor is damaged beyond repair; adjust defenses accordingly). If the target's armor is magical, the target must roll a hard save. On a success, the armor isn't affected. This effect is permanent until the creature obtains new armor. Each time the rust monster obliterator destroys heavy armor with this attack, it gains a different random demon ability until the end of the battle."],
+    abilities: ["Corrupting Body: Whenever a creature hits the rust monster with a melee weapon, the attacker must roll a save; on a failure, the weapon is destroyed. Magic weapons require an easy save instead. This effect is permanent.", "Rust's Targets: Magic items wielded by creatures with 100 HP or more are not affected by the rust monster's ability to destroy items (but the target still takes damage).", "Saving Quirks: Before rolling a save for a magic item, a PC can gain a +5 bonus to the save by agreeing to roleplay the item's quirk as a huge element of their personality until the next full heal up. Failure to live up to this roleplaying agreement means that the item hasn't received the support it needs from its owner to survive the rust monster's destructive effects, and the item is destroyed at the end of the next battle even if its owner successfully saved.", "[Nastier Special] Tail Whirligig: When an enemy targets the rust monster obliterator with a spell, the rust monster rolls a save. On a success, the spell has no effect on the rust monster. If the rust monster is Staggered, it must roll a hard save instead."],
+    isStaggered: false,
+    id: 292
+},
 
 // SAHUAGIN
 
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 293
+},
 
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 294
+},
+
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 295
+},
+
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 296
+},
+
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 297
+},
+
+{
+    name: "",
+    level: ,
+    type: "",
+    initiative: "",
+    ac: ,
+    pd: ,
+    md: ,
+    hp: ,
+    health: ,
+    attacks: [""],
+    abilities: [""],
+    isStaggered: false,
+    id: 298
+},
 
 // SHADOW DRAGONS
 
